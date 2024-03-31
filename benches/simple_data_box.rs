15c836866044ca6c56dcd1feb33812bfa6117c89
Eric Scouten <scouten@adobe.com>
2024-03-31 12:51:40 -0700

    Restore benchmarks

--- benches/simple_data_box.rs
+++ benches/simple_data_box.rs
@@ -2,17 +2,18 @@ use codspeed_criterion_compat::{criterion_group, criterion_main, Criterion};
 use hex_literal::hex;
 use jumbf::parser::DataBox;
 
-const SIMPLE_BOX: [u8; 38] = hex!(
+const SIMPLE_BOX: &[u8] = hex!(
     "00000026" // box size
     "6a756d64" // box type = 'jumd'
     "00000000000000000000000000000000" // UUID
     "03" // toggles
     "746573742e64657363626f7800" // label
-);
+)
+.as_slice();
 
 pub fn simple_parse(c: &mut Criterion) {
     c.bench_function("simple data box", |b| {
-        b.iter(|| DataBox::from_slice(&SIMPLE_BOX).unwrap());
+        b.iter(|| DataBox::from_source(SIMPLE_BOX).unwrap());
     });
 }
 
